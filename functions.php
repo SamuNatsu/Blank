680cfa3d1fc443115de71b28a2fc0033a8cac5df
SamuNatsu <samunatsu@qq.com>
2023-02-01 16:18:45 +0800

    Update: Better option menu

--- functions.php
+++ functions.php
@@ -1,37 +1,80 @@
 <?php
+
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
-require_once 'var/TinyHtmlMinifier.php';
-require_once 'var/ThemeView.php';
+require_once('var/TinyHtmlMinifier.php');
 
-function themeConfig($form)
+require_once('var/Backup.php');
+require_once('var/Update.php');
+require_once('var/View.php');
+
+// Auto update
+\Blank\Update::autoUpdate();
+
+/**
+ * Theme config
+ * 
+ * @param \Typecho\Widget\Helper\Form $form
+ * 
+ * @return void
+ */
+function themeConfig(\Typecho\Widget\Helper\Form $form): void
 {
-	$maintainMode = new \Typecho\Widget\Helper\Form\Element\Radio(
-		'maintainMode',
+	// Go to top button html
+	$btn_top = '<a href="#bt-a-top" title="' . _t('回到顶部') . '"><img class="bt-btn-top" src="' . \Utils\Helper::options()->themeUrl . '/svg/top.svg"></a>';
+
+	// Backup action handle
+	\Blank\Backup::handle();
+
+	// Render header
+?>
+<link rel="stylesheet" href="<?php \Utils\Helper::options()->themeUrl('css/options-theme.css?v=0.1.0'); ?>">
+<h2 class="bt-title" id="bt-a-top"><?php _e('Blank 主题配置'); ?></h2>
+
+<?php \Blank\Update::updatedBanner(); ?>
+
+<div class="bt-banner bt-banner-info">
+	<h3><?php _e('快速跳转'); ?></h3>
+	<p>
+		<a href="#bt-a-1"><?php _e('全局'); ?></a>，
+		<a href="#bt-a-2"><?php _e('镜像'); ?></a>，
+		<a href="#bt-a-3"><?php _e('侧边栏'); ?></a>，
+		<a href="#bt-a-4"><?php _e('页脚'); ?></a>，
+		<a href="#bt-a-5"><?php _e('自定义'); ?></a>
+	</p>
+</div>
+
+<?php \Blank\Backup::render(); ?>
+
+<h3 class="bt-sub-title" id="bt-a-1"><?php _e('全局'); ?><?= $btn_top; ?></h3>
+<?php
+	// Form items
+	$isMaintaining = new \Typecho\Widget\Helper\Form\Element\Radio(
+		'isMaintaining',
 		[
 			'on'	=> _t('开启'),
 			'off'	=> _t('关闭')
 		],
 		'off',
-		_t('【全局】维护模式'),
+		_t('维护模式'),
 		_t('启动维护模式后，只有登录的管理员才能进入站点')
 	);
-	$form->addInput($maintainMode);
+	$form->addInput($isMaintaining);
 
-	$minify = new \Typecho\Widget\Helper\Form\Element\Radio(
-		'minify',
+	$isMinified = new \Typecho\Widget\Helper\Form\Element\Radio(
+		'isMinified',
 		[
 			'on'	=> _t('开启'),
 			'off'	=> _t('关闭')
 		],
 		'off',
-		_t('【全局】HTML 压缩'),
-		_t('启动 HTML 压缩以减少服务器传输压力<br>注 1：使用 URL 引用的文件不会参与压缩，请使用对应的 min 版本<br>注 2：如果网页出现了意料外的表现，请检查是否有不规范的 HTML 代码，或关闭压缩<br>注 3：已知在 HTML 中嵌入 JS 代码时，使用箭头函数会导致压缩错误并破坏脚本完整性，请使用 URL 引用该类脚本或使用 function 替代箭头函数')
+		_t('HTML 压缩'),
+		_t('启动 HTML 压缩以减少服务器传输压力<br>注 1：HTML 压缩只会作用于前台<br>注 2：使用 URL 引用的文件不会参与压缩，请使用对应的 min 版本<br>注 3：已知在前台 HTML 中出现非标签的左/右尖括号时，会导致标签错误地提前开始/终止，这是压缩后端的一个 bug，未来可能会修复，取决于后端的原作者，或我们在未来开发自己的压缩后端代替')
 	);
-	$form->addInput($minify);
+	$form->addInput($isMinified);
 
-	$ccLicense = new \Typecho\Widget\Helper\Form\Element\Select(
-		'ccLicense',
+	$defaultCC = new \Typecho\Widget\Helper\Form\Element\Select(
+		'defaultCC',
 		[
 			'zero'		=> _t('CC0 公共领域'),
 			'by'		=> _t('BY 署名'),
@@ -42,39 +85,39 @@ function themeConfig($form)
 			'by-nc-nd'	=> _t('BY-NC-ND 署名-非商业使用-禁止演绎')
 		],
 		'by-sa',
-		_t('【全局】默认知识共享许可协议'),
-		_t('推荐且默认使用 BY-SA 署名-相同方式共享<br>您可以在 <a href="https://creativecommons.org/licenses/" target="_blank">https://creativecommons.org/licenses/</a> 查看各种协议的规定<br>如果您想为某一篇文章或页面指定特定的知识共享许可协议，请在该文章或页面中添加自定义字段“cc”，并填写如下对应的协议字符串（不包括引号）：<br>&emsp;“zero”：CC0 公共领域<br>&emsp;“by”：BY 署名<br>&emsp;“by-sa”：BY-SA 署名-相同方式共享<br>&emsp;“by-nc”：BY-NC 署名-非商业使用<br>&emsp;“by-nd”：BY-ND 署名-禁止演绎<br>&emsp;“by-nc-sa”：BY-NC-SA 署名-非商业使用-相同方式共享<br>&emsp;“by-nc-nd”：BY-NC-ND 署名-非商业使用-禁止演绎<br>若“cc”字段无法匹配上面的协议字符串，则该文章或页面不会显示知识共享许可协议（可能会为您的维权带来一定麻烦）')
+		_t('默认知识共享许可协议'),
+		_t('推荐且默认使用 BY-SA 署名-相同方式共享<br>您可以在 <a href="https://creativecommons.org/licenses/" target="_blank">https://creativecommons.org/licenses/</a> 查看各种协议的规定<br>如果您想为某一篇文章或页面指定特定的知识共享许可协议，请在该文章或页面中添加自定义字段“cc”，并填写如下对应的协议字符串（不包括引号）：<br>&emsp;“zero”：CC0 公共领域<br>&emsp;“by”：BY 署名<br>&emsp;“by-sa”：BY-SA 署名-相同方式共享<br>&emsp;“by-nc”：BY-NC 署名-非商业使用<br>&emsp;“by-nd”：BY-ND 署名-禁止演绎<br>&emsp;“by-nc-sa”：BY-NC-SA 署名-非商业使用-相同方式共享<br>&emsp;“by-nc-nd”：BY-NC-ND 署名-非商业使用-禁止演绎<br>若“cc”字段无法匹配上面的协议字符串，则该文章或页面不会显示知识共享许可协议')
 	);
-	$form->addInput($ccLicense);
+	$form->addInput($defaultCC);
 
-	$favicon = new \Typecho\Widget\Helper\Form\Element\Text(
-		'favicon',
+	$faviconUrl = new \Typecho\Widget\Helper\Form\Element\Text(
+		'faviconUrl',
 		null,
 		null,
-		_t('【全局】网站图标链接'),
-		_t('留空则根据浏览器规则自动调用 /favicon.ico，可能会产生恼人的 404 错误')
+		_t('网站图标链接'),
+		'<hr><h3 class="bt-sub-title" id="bt-a-2">' . _t('镜像') . $btn_top . '</h3>'
 	);
-	$form->addInput($favicon);
+	$form->addInput($faviconUrl);
 
-	$_gravatarServiceWarning = '';
+	$_gravatarMirrorWarning = '';
 	if (defined('__TYPECHO_GRAVATAR_PREFIX__'))
 	{
-		$_gravatarServiceWarning = _t('<br><span style="color:red">警告：检测到 PHP 常量 __TYPECHO_GRAVATAR_PREFIX__ 已被定义为 "') . __TYPECHO_GRAVATAR_PREFIX__ . _t('"，导致镜像替换无法进行，请注释或删除相关代码</span>');
+		$_gravatarMirrorWarning = _t('<br><span style="color:red">警告：检测到 PHP 常量 __TYPECHO_GRAVATAR_PREFIX__ 已被定义为 "') . __TYPECHO_GRAVATAR_PREFIX__ . _t('"，导致镜像替换无法进行，请注释或删除相关代码</span>');
 	}
-	$gravatarService = new \Typecho\Widget\Helper\Form\Element\Text(
-		'gravatarService',
+	$gravatarMirror = new \Typecho\Widget\Helper\Form\Element\Text(
+		'gravatarMirror',
 		null,
 		'https://gravatar.loli.net/avatar/',
-		_t('【全局】Gravatar 服务镜像替换'),
-		_t('推荐且默认使用 https://gravatar.loli.net/avatar/，当然你可以换成其他 Gravatar 镜像') . $_gravatarServiceWarning
+		_t('Gravatar 镜像'),
+		_t('推荐且默认使用 https://gravatar.loli.net/avatar/，当然你可以换成其他 Gravatar 镜像') . $_gravatarMirrorWarning
 	);
-	$form->addInput($gravatarService);
+	$form->addInput($gravatarMirror);
 
 	$modernNormalize = new \Typecho\Widget\Helper\Form\Element\Text(
 		'modernNormalize',
 		null,
 		null,
-		_t('【全局】Modern Normalize 镜像替换'),
+		_t('Modern Normalize 镜像'),
 		_t('留空则使用主题自带的 modern-normalize.min.css（v1.1.0）<br>您可以在 <a href="https://github.com/sindresorhus/modern-normalize" target="_blank">https://github.com/sindresorhus/modern-normalize</a> 查看该样式表详情')
 	);
 	$form->addInput($modernNormalize);
@@ -83,7 +126,7 @@ function themeConfig($form)
 		'pjaxService',
 		null,
 		null,
-		_t('【全局】Pjax 镜像替换'),
+		_t('Pjax 镜像'),
 		_t('留空则使用主题自带的 pjax.min.js（v0.2.8）<br>注：该 Pjax 不是 jQuery 插件，是独立模块')
 	);
 	$form->addInput($pjaxService);
@@ -92,7 +135,7 @@ function themeConfig($form)
 		'hljsScript',
 		null,
 		null,
-		_t('【全局】【Highlight.js】脚本镜像替换'),
+		_t('Highlight.js 镜像'),
 		_t('留空则使用主题自带的 highlight.min.js（v11.7.0）')
 	);
 	$form->addInput($hljsScript);
@@ -101,7 +144,7 @@ function themeConfig($form)
 		'hljsNumber',
 		null,
 		null,
-		_t('【全局】【Highlight.js】行号插件镜像替换'),
+		_t('Highlight.js 行号插件镜像'),
 		_t('留空则使用主题自带的 highlightjs-line-numbers.min.js（v2.8.0）<br>您可以在 <a href="https://github.com/wcoder/highlightjs-line-numbers.js" target="_blank">https://github.com/wcoder/highlightjs-line-numbers.js</a> 查看该插件的相关详情')
 	);
 	$form->addInput($hljsNumber);
@@ -110,7 +153,7 @@ function themeConfig($form)
 		'katexCss',
 		null,
 		null,
-		_t('【全局】【Katex】样式表镜像替换'),
+		_t('Katex 样式表镜像'),
 		_t('留空则使用主题自带的 katex.min.css（v0.16.4）')
 	);
 	$form->addInput($katexCss);
@@ -119,7 +162,7 @@ function themeConfig($form)
 		'katexScript',
 		null,
 		null,
-		_t('【全局】【Katex】脚本镜像替换'),
+		_t('Katex 脚本镜像'),
 		_t('留空则使用主题自带的 katex.min.js（v0.16.4）')
 	);
 	$form->addInput($katexScript);
@@ -128,8 +171,8 @@ function themeConfig($form)
 		'katexAuto',
 		null,
 		null,
-		_t('【全局】【Katex】自动渲染脚本镜像替换'),
-		_t('留空则使用主题自带的 auto-render.min.js（v0.16.4）<style>hr{border:0;border-top:2px dashed #aaa;heigh:0;margin:2em 0}</style><hr/>')
+		_t('Katex 自动渲染脚本镜像'),
+		_t('留空则使用主题自带的 auto-render.min.js（v0.16.4）') . '<hr><h3 class="bt-sub-title" id="bt-a-3">' . _t('侧边栏') . $btn_top . '</h3>'
 	);
 	$form->addInput($katexAuto);
 
@@ -137,7 +180,7 @@ function themeConfig($form)
 		'customGravatar',
 		null,
 		null,
-		_t('【侧边栏】头像图片链接'),
+		_t('头像图片链接'),
 		_t('留空则使用 Typecho 默认定义的头像链接（邮箱 + Gravatar 服务）')
 	);
 	$form->addInput($customGravatar);
@@ -146,7 +189,7 @@ function themeConfig($form)
 		'gravatarMail',
 		null,
 		null,
-		_t('【侧边栏】【Gravatar】博主邮箱'),
+		_t('Gravatar 邮箱'),
 		_t('由于 Typecho 是多用户系统，而 Blank 主题是单用户主题，所以调用 Typecho 自带的 Gravatar 生成会有指向不明确的问题，因此请手动设置')
 	);
 	$form->addInput($gravatarMail);
@@ -155,7 +198,7 @@ function themeConfig($form)
 		'gravatarName',
 		null,
 		null,
-		_t('【侧边栏】【Gravatar】博主名称'),
+		_t('博主名称'),
 		_t('由于 Typecho 是多用户系统，而 Blank 主题是单用户主题，所以调用 Typecho 自带的显示名称会有指向不明确的问题，因此请手动设置')
 	);
 	$form->addInput($gravatarName);
@@ -164,7 +207,7 @@ function themeConfig($form)
 		'biliUrl',
 		null,
 		null,
-		_t('【侧边栏】【链接】哔哩哔哩')
+		_t('哔哩哔哩主页链接')
 	);
 	$form->addInput($biliUrl);
 
@@ -172,7 +215,7 @@ function themeConfig($form)
 		'giteeUrl',
 		null,
 		null,
-		_t('【侧边栏】【链接】码云')
+		_t('码云主页链接')
 	);
 	$form->addInput($giteeUrl);
 
@@ -180,7 +223,7 @@ function themeConfig($form)
 		'githubUrl',
 		null,
 		null,
-		_t('【侧边栏】【链接】Github')
+		_t('Github 主页链接')
 	);
 	$form->addInput($githubUrl);
 
@@ -188,7 +231,7 @@ function themeConfig($form)
 		'mailUrl',
 		null,
 		null,
-		_t('【侧边栏】【链接】邮箱')
+		_t('Mailto 邮箱')
 	);
 	$form->addInput($mailUrl);
 
@@ -196,7 +239,7 @@ function themeConfig($form)
 		'telegramUrl',
 		null,
 		null,
-		_t('【侧边栏】【链接】电报')
+		_t('电报主页链接')
 	);
 	$form->addInput($telegramUrl);
 
@@ -204,7 +247,7 @@ function themeConfig($form)
 		'twitterUrl',
 		null,
 		null,
-		_t('【侧边栏】【链接】推特')
+		_t('推特主页链接')
 	);
 	$form->addInput($twitterUrl);
 
@@ -212,8 +255,8 @@ function themeConfig($form)
 		'weiboUrl',
 		null,
 		null,
-		_t('【侧边栏】【链接】微博'),
-		'<hr/>'
+		_t('微博主页链接'),
+		'<hr><h3 class="bt-sub-title" id="bt-a-4">' . _t('页脚') . $btn_top . '</h3>'
 	);
 	$form->addInput($weiboUrl);
 
@@ -221,8 +264,8 @@ function themeConfig($form)
 		'icp',
 		null,
 		null,
-		_t('【页脚】ICP 备案信息'),
-		'<hr/>'
+		_t('ICP 备案信息'),
+		'<hr><h3 class="bt-sub-title" id="bt-a-5">' . _t('自定义') . $btn_top . '</h3>'
 	);
 	$form->addInput($icp);
 
@@ -230,7 +273,7 @@ function themeConfig($form)
 		'customHead',
 		null,
 		'',
-		_t('【自定义】头部标签附加代码'),
+		_t('头部标签附加代码'),
 		_t('在 &lt;head&gt;&lt;/head&gt; 标签中追加代码')
 	);
 	$form->addInput($customHead);
@@ -239,7 +282,7 @@ function themeConfig($form)
 		'customFooter',
 		null,
 		'',
-		_t('【自定义】页脚附加代码'),
+		_t('页脚附加代码'),
 		_t('在 &lt;footer&gt;&lt;/footer&gt; 标签中追加代码')
 	);
 	$form->addInput($customFooter);
@@ -248,7 +291,7 @@ function themeConfig($form)
 function minifyBegin($options): void
 {
 	ob_start('ob_gzhandler');
-	if ($options->minify === 'on')
+	if ($options->isMinified === 'on')
 	{
 		ob_start();
 	}
@@ -256,7 +299,7 @@ function minifyBegin($options): void
 
 function minifyEnd($options): void
 {
-	if ($options->minify === 'on')
+	if ($options->isMinified === 'on')
 	{
 		$html = ob_get_clean();
 		$minifier = new \Minifier\TinyHtmlMinifier([]);
